170d1603b6f40bf83e46e3d52e884fd7d739f65f
Juan Carlos Ballesteros <jcballesteroshermida@gmail.com>
2025-06-08 13:26:34 +0200

    Basic app class with logger

---
+++ src/main/java/telegrambot/App.java
@@ -0,0 +1,13 @@
+package telegrambot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hola desde Logback");
+    }
+}
